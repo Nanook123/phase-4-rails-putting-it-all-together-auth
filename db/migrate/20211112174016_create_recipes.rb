b827ab703a7ae450981ee3711ead2430c1e0c48e
Joshua Allford <joshallfordnanook@gmail.com>
2021-11-12 18:35:09 +0000

    commit tree 29e8d5f73ea8ade657fde14806e61698d299688b

---
+++ db/migrate/20211112174016_create_recipes.rb
@@ -0,0 +1,12 @@
+class CreateRecipes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.string :instructions
+      t.string :minutes_to_complete
+      t.string :integer
+
+      t.timestamps
+    end
+  end
+end
